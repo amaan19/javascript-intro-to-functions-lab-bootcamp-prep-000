d0cf45d7fae04826ed622373fbcb6c1f525a3043
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-10-22 12:54:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase () === string) {
     return 'YES INDEED!'
-  } else if {
-    string.toLowerCase () === string)
+  } else if string.toLowerCase () === string) {
+    console.log("I can't hear you!")
+  } else if string === "I love you" {
+    console.log("I love you, too")
+  } else 
   }
-}
+
